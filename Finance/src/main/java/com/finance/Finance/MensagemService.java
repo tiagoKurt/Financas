d4c76bf9c99f6acfeffa7577e3ab3ab5b083f3a1
Tiago Marques de Oliveira <105756442+tiagoKurt@users.noreply.github.com>
2023-09-22 11:57:21 -0300

    piloto

---
+++ Finance/src/main/java/com/finance/Finance/MensagemService.java
@@ -0,0 +1,12 @@
+package com.finance.Finance;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("production")
+public class MensagemService{
+	@Value("${application.name}")
+	private String appName;
+}
